454763f1046d8434ca82ef7cce4542ffb2f21b15
Jpglimbo <Jaypeg43@gmail.com>
2023-07-14 21:21:58 -0300

    Header

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -15,85 +15,81 @@ export default class MenuExampleStackable extends Component {
    
     return (
       <div>
-         <Menu inverted>
-  
-        <Menu.Item icon='box'
-          name='Adicionar Produto'
-          active={activeItem === 'produtos'}
-          onClick={this.handleItemClick}
-          as={Link}
-          to='/form-produto'
-        />
-        <Menu.Item icon='tasks'
-          name='Produtos Cadastrados'
-          active={activeItem === 'editarProduto'}
-          onClick={this.handleItemClick}
-          as={Link}
-          to='/list-produto'
-        />
-        
-      </Menu>
-       
-        <div >
-          
-        <Menu position='right' 
-        stackable size='huge' 
-        color='green' inverted 
-        >
-          <Menu.Menu >
-          <Menu.Item position='right' >
-            <img alt="logo" src='/iconeApp.png'       
-            /> OOK!
-          </Menu.Item>
+      <Menu inverted>
 
-          <Menu.Item icon='home'
-            name='Página Inicial'
-            active={activeItem === 'editarProduto'}
-            onClick={this.handleItemClick}
-            as={Link}
-            to='/'
+     <Menu.Item
+    
+     icon='box'
+       name='Adicionar Produto'
+       active={activeItem === 'produtos'}
+       onClick={this.handleItemClick}
+       as={Link}
+       to='/form-produto'
+     />
+     <Menu.Item icon='tasks'
+       name='Produtos Cadastrados'
+       active={activeItem === 'editarProduto'}
+       onClick={this.handleItemClick}
+       as={Link}
+       to='/list-produto'
+     />
+   </Menu>
+    
+     <div>
+     
+     <Menu position='left' stackable size='huge' color='green' inverted >
+     <Menu.Item  >
+       <img alt="logo" src='/iconeApp.png'       
+       /> OOK!
+     </Menu.Item>
 
-          />
+     <Menu.Item position='left'
+      icon='home'
+       name='Página Inicial'
+       active={activeItem === 'editarProduto'}
+       onClick={this.handleItemClick}
+       as={Link}
+       to='/'
 
-          <Menu.Item>
-          <Input className='icon' icon='search' placeholder='Pesquisar...' />
-          
-          </Menu.Item>
+     />
+    
+     <Menu.Item>
+     <Input className='icon' icon='search' placeholder='Pesquisar...' />
+     
+     </Menu.Item>
 
-        <Menu.Item>
-        <Dropdown item text='Categorias' >
-        <Dropdown.Menu>
-        <Dropdown.Item>Alimentos</Dropdown.Item>
-        <Dropdown.Item>Tecnologia</Dropdown.Item>
-        <Dropdown.Item>Casa</Dropdown.Item>
-        </Dropdown.Menu>
-        </Dropdown>
-        </Menu.Item>
+     <Menu.Item>
+     <Dropdown item text='Categorias' >
+     <Dropdown.Menu>
+     <Dropdown.Item>Alimentos</Dropdown.Item>
+     <Dropdown.Item>Tecnologia</Dropdown.Item>
+     <Dropdown.Item>Casa</Dropdown.Item>
+     </Dropdown.Menu>
+     </Dropdown>
+     </Menu.Item>
 
-        
-        <Menu.Item icon='user circle'
-          name='Entrar'
-          active={activeItem === 'login'}
-          onClick={this.handleItemClick}
-          as={Link}
-          to='/cadastro'
-        />
+     <Menu.Menu position='right'>
+     <Menu.Item icon='user circle'
+       name='Entrar'
+       active={activeItem === 'login'}
+       onClick={this.handleItemClick}
+       as={Link}
+       to='/cadastro'
+     />
 
-        <Menu.Item icon='shopping basket'
-          name='Registre sua Loja'
-          active={activeItem === 'loja'}
-          onClick={this.handleItemClick}
-          as={Link}
-          to='/form-loja'
-        />
-        </Menu.Menu>
-        </Menu>
-        </div>
-        <br></br>
+     <Menu.Item icon='shopping basket'
+       name='Registre sua Loja'
+       active={activeItem === 'loja'}
+       onClick={this.handleItemClick}
+       as={Link}
+       to='/form-loja'
+     />
+     </Menu.Menu>
+     </Menu>
+     </div>
+     <br></br>
 
-      </div>
-
-
+   </div>
 
     )
   }
